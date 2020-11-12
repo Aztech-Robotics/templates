2a923d5f91b5c442d2605bd24de9e7694fdeae3f
Valeria-Flores <69869351+Valeria-Flores@users.noreply.github.com>
2020-11-12 14:45:11 -0600

    Page

---
+++ script.js
@@ -0,0 +1,72 @@
+$(function(){
+    let start = 0;
+    let end = $(".num").html();
+    let speed = 10;
+    
+    setInterval(function(){
+        if(start<end){
+            start++;
+        }
+        $(".num").html(start);
+    }, speed);
+});
+$(function(){
+    let start = 0;
+    let end = $(".num2").html();
+    let speed = 10;
+    
+    setInterval(function(){
+        if(start<end){
+            start++;
+        }
+        $(".num2").html(start);
+    }, speed);
+});
+$(function(){
+    let start = 0;
+    let end = $(".num3").html();
+    let speed = 10;
+    
+    setInterval(function(){
+        if(start<end){
+            start++;
+        }
+        $(".num3").html(start);
+    }, speed);
+});
+$(function(){
+    let start = 0;
+    let end = $(".num4").html();
+    let speed = 10;
+    
+    setInterval(function(){
+        if(start<end){
+            start++;
+        }
+        $(".num4").html(start);
+    }, speed);
+});
+$(function(){
+    let start = 0;
+    let end = $(".num5").html();
+    let speed = 10;
+    
+    setInterval(function(){
+        if(start<end){
+            start++;
+        }
+        $(".num5").html(start);
+    }, speed);
+});
+$(function(){
+    let start = 0;
+    let end = $(".num6").html();
+    let speed = 10;
+    
+    setInterval(function(){
+        if(start<end){
+            start++;
+        }
+        $(".num6").html(start);
+    }, speed);
+});
